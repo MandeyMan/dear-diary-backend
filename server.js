9fe079d2e239007e0a66e919f03af54d1bbd0538
MandeyMan <pinkuatthe9532@gmail.com>
2024-11-04 23:07:54 +0530

    Update CORS configuration

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRouter');
 const diaryRoutes = require('./routes/diaryRoutes');
@@ -9,23 +9,34 @@ require('dotenv').config();
 
 const app = express();
 
+// CORS configuration
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://dear-diary-frontend.onrender.com',
+];
+
 app.use(cors({
-     origin: 'http://localhost:3000', // Allow your React app's origin
-     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-     credentials: true, // Include credentials in the request if needed
-   }));
-   
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    credentials: true, // Include credentials in the request if needed
+}));
 
 app.use(express.json());
 
 connectDB();
-//routes
-app.use('/api/auth' , authRoutes);
-app.use('/api/diaries' , diaryRoutes);
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/diaries', diaryRoutes);
 app.use('/api', emailRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT , ()=>{
-     console.log(`App started succefully ${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`App started successfully on port ${PORT}`);
+});
